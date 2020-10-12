aa440cfe3292e47d8308fc1fb166c0c18c9a4c0a
Bene ImI <beneimi99@gmail.com>
2020-10-12 11:16:17 +0100

    Translate theme button and change text sizes.

--- rootfolder/js/theme-changer.js
+++ rootfolder/js/theme-changer.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     if(localStorage.getItem('theme') == 'dark'){
         document.getElementById('stylesheet').setAttribute('href', 'css/style_dark.css');
+        document.getElementById('theme_switch').innerHTML = 'Világos téma' 
     }
 };
 
@@ -8,9 +9,13 @@ function changeTheme(){
     if(localStorage.getItem('theme') == 'light'){
         document.getElementById('stylesheet').setAttribute('href', 'css/style_dark.css');
         localStorage.setItem('theme', 'dark');
+        document.getElementById('theme_switch').innerHTML = 'Világos téma' 
+
     }
     else{
         document.getElementById('stylesheet').setAttribute('href', 'css/style.css');
         localStorage.setItem('theme', 'light');
+        document.getElementById('theme_switch').innerHTML = 'Sötét téma' 
+
     }
 }
