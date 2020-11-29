e90ffdeaf030209acc057cb6c214c451e2aaa170
ST1G1M8 <stigimate@gmail.com>
2020-11-29 14:17:21 +0100

    Added profile.php

--- rootfolder/profile.php
+++ rootfolder/profile.php
@@ -3,12 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profil</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" id="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="img/png" href="images/favicon.jpg">
+    <script src="js/theme-changer.js"></script>
 </head>
 <body>
-    <?php
+    <header id="home-page">
+        <nav class="nav-bar">
+            <ul>
+                <li><a href="index.php">Főoldal</a></li>
+                <li><a href="index.php">Galéria</a></li>
+                <li><a href="index.php">Rólunk</a></li>
+                <li><a href="profile.php">Profil</a></li>
+                <button id="theme_switch" type="submit" class="stylebutton" onclick="changeTheme()">Sötét téma</button>
+            </ul>
+        </nav>
+        
+        <div class="nav-bar-bottom-border"></div>
+
+        <div class="main-wrapper">
+            <section class="descripion">
+            <h3>Üdvözöllek a Sörök Birodalmában</h3>
+                <?php
+                    echo "Aktuális dátum: " . date("Y-m-d") . "<br>";
+                ?>
+            <h2>Kedvencek:</h2>
+            </section>
+        </div>
+    </header>
     
-    echo "hallo";
-    ?>
+
 </body>
 </html>
